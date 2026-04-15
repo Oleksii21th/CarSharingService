28ca1c5497b141e1c2f32489286edc493ed53489
Admin <admin@>
2026-04-15 19:15:12 +0200

    Avoid NoSuchElementException and change column name in test.

--- src/test/resources/database/payment/add-default-payments.sql
+++ src/test/resources/database/payment/add-default-payments.sql
@@ -1,2 +1,2 @@
-INSERT INTO payment (id, status, type, rental_id, session_url, session_id, amount_to_pay, is_deleted)
+INSERT INTO payment (id, status, type, rental_id, session_url, session_id, amount, is_deleted)
 VALUES (2, 'PENDING', 'PAYMENT', 2, 'Test1', 'session1', 2, false);
