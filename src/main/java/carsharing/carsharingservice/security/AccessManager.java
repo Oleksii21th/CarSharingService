97d96d75d9f0804a24bccf16478bf2202ee079e4
Admin <admin@>
2026-04-26 10:18:51 +0200

    Removing direct user casting from Authentication

--- src/main/java/carsharing/carsharingservice/security/AccessManager.java
+++ src/main/java/carsharing/carsharingservice/security/AccessManager.java
@@ -1,12 +1,21 @@
 package carsharing.carsharingservice.security;
 
 import carsharing.carsharingservice.model.User;
+import carsharing.carsharingservice.repository.UserRepository;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AccessManager {
+
+    private final UserRepository userRepository;
+
+    public AccessManager(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public boolean isManager(Authentication authentication) {
         return authentication.getAuthorities().stream()
                 .anyMatch(a ->
@@ -18,19 +27,25 @@ public class AccessManager {
             return resourceUserId;
         }
 
-        boolean isManager = isManager(authentication);
-
-        return isManager ? null : ((User) authentication.getPrincipal()).getId();
+        return isManager(authentication) ? null : getCurrentUserId(authentication);
     }
 
     public void checkOwnerOrManager(Authentication authentication, Long resourceUserId) {
-        User currentUser = (User) authentication.getPrincipal();
+        Long currentUserId = getCurrentUserId(authentication);
 
-        boolean isOwner = resourceUserId != null && resourceUserId.equals(currentUser.getId());
+        boolean isOwner = resourceUserId != null && resourceUserId.equals(currentUserId);
         boolean isManager = isManager(authentication);
 
         if (!isOwner && !isManager) {
             throw new AccessDeniedException("Access denied");
         }
+    }
+
+    private Long getCurrentUserId(Authentication authentication) {
+        String email = authentication.getName();
+        User user = userRepository.findByEmail(email).orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email: " + email));
+
+        return user.getId();
     }
 }
